d7e1d48066cbc05ace1642b42cf5f210daff4a9d
MorganShip <morganshipsides@outlook.com>
2018-10-11 22:46:52 +0100

    Created Example Use

--- Task2/Task2.java
+++ Task2/Task2.java
@@ -1,7 +1,14 @@
+import java.util.ArrayList;
+import java.io.IOException;
+
 public class Task2
 {
-	public static void main(String[] args) 
+	public static void main(String[] args) throws IOException
 	{
+		Interpreter II = new Interpreter();
+
+		ArrayList<Token> Tokens = Tokenizer.TokenizeFile(TextFileHelper.GetLines("C:/Users/Morgan/Desktop/Programming Projects/JavaStuff/SpaceCadets/Task2/TestOne.txt")); // This should be dynamic
 		
+		II.Run(Tokens);
 	}
 }
--- Task2/Tokenizer.java
+++ Task2/Tokenizer.java
@@ -17,45 +17,47 @@ public class Tokenizer
 	private static Token TokenizeLine(String Line) 
 	{
 		Pattern incrReg = Pattern.compile("incr\\s[a-zA-Z]+[a-zA-Z0-9]*\\s?[0-9]*;");
-		Pattern decrReg = Pattern.compile("incr\\s[a-zA-Z]+[a-zA-Z0-9]*\\s?[0-9]*;");
+		Pattern decrReg = Pattern.compile("decr\\s[a-zA-Z]+[a-zA-Z0-9]*\\s?[0-9]*;");
 		Pattern clearReg = Pattern.compile("clear\\s[a-zA-Z]+[a-zA-Z0-9]*;");
 		Pattern notReg = Pattern.compile("[a-zA-Z]+[a-zA-Z0-9]*\\s not \\s?[0-9]*");
 		Pattern whiledoReg = Pattern.compile("while\\s[a-zA-Z0-9\\s]*\\sdo;");
+		Pattern endReg = Pattern.compile("end;");
 
 		Matcher incrMatch = incrReg.matcher(Line);
 		Matcher decrMatch = decrReg.matcher(Line);
 		Matcher clearMatch = clearReg.matcher(Line);
 		Matcher notMatch = notReg.matcher(Line);
 		Matcher whiledoMatch = whiledoReg.matcher(Line);
+		Matcher endMatch = endReg.matcher(Line);
 
 		if (incrMatch.matches()) {
-			Line = Line.substring(6, Line.length() - 1);
+			Line = Line.substring(5, Line.length() - 1);
 			String[] parts = Line.split(" ");
 
 			if (parts.length == 1) {
-				return new Token("incr", parts[0]);
+				return new Token("incr", parts[0], 1);
 			} else {
 				return new Token("incr", parts[0], Integer.parseInt(parts[1]));
 			}
 		} else if (decrMatch.matches()) {
-			Line = Line.substring(6, Line.length() - 1);
+			Line = Line.substring(5, Line.length() - 1);
 			String[] parts = Line.split(" ");
 
 			if (parts.length == 1) {
-				return new Token("decr", parts[0]);
+				return new Token("decr", parts[0], 1);
 			} else {
 				return new Token("decr", parts[0], Integer.parseInt(parts[1]));
 			}
 		} else if (clearMatch.matches()) {
-			Line = Line.substring(7, Line.length() - 1);
+			Line = Line.substring(6, Line.length() - 1);
 
 			return new Token("clear", Line);
 		} else if (whiledoMatch.matches()) {
-			Line = Line.substring(7, Line.length() - 4);
+			Line = Line.substring(6, Line.length() - 4);
 			String[] parts = Line.split(" not ");
 
 			return new Token("not", parts[0], Integer.parseInt(parts[1]));
-		} else if (Line == "end;") {
+		} else if (endMatch.matches()) {
 			return new Token("end");
 		} else {
 			return null;
